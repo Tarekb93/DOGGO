32fd98ce9eb1c22ba084fc38fc815102d03d99c4
Tarek Bashir <tarekb93@gmail.com>
2020-08-27 12:36:54 +0300

    add port on server

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 const app = require("./app.js");
-
+var PORT = process.env.PORT || 3000;
 const port = 5555;
 app.listen(port, () => {
   console.log(`Our app is running on http://localhost:${port}`);
